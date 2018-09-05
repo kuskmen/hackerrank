41e7f3866b168a5092661d8d94eabe272030af76
Yanislav Vasilev <ianislav_vasilev@abv.bg>
2018-09-05 22:29:52 +0300

    Submission for rotating left the array.

---
+++ src/interview preparation kit/RotateLeft.cs
@@ -0,0 +1,24 @@
+﻿namespace HackerRank.interview_preparation_kit
+{
+    public class RotateLeft
+    {
+        public static int[] rotateLeft(int[] a, int d)
+        {
+            var b = new int[a.Length];
+            for (var i = 0; i < a.Length; i++)
+            {
+                var offset = i - d;
+
+                if (offset < 0)
+                {
+                    b[a.Length + offset] = a[i];
+                    continue;
+                }
+
+                b[offset] = a[i];
+            }
+
+            return b;
+        }
+    }
+}

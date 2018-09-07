88e8bad70c12c4a0915f5312535b7a6e8ea9ee2c
Yanislav Vasilev <ianislav_vasilev@abv.bg>
2018-09-07 23:45:00 +0300

    Mark and toys submission.

---
+++ src/interview preparation kit/MarkAndToys.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HackerRank.interview_preparation_kit
+{
+    public class MarkAndToys
+    {
+        public static int maximumToys(int[] prices, int k)
+        {
+            Array.Sort(prices);
+            var i = 0;
+
+            while (k > 0 && i < prices.Length - 1)
+            {
+                k -= prices[i];
+                if (k > 0) i++;
+            }
+
+            return i;
+        }
+    }
+}

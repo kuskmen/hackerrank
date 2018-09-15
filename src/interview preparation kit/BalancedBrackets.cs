a93e1d15fe4ca96bb5c80dbeac15586acd54b6c2
Yanislav Vasilev <ianislav_vasilev@abv.bg>
2018-09-15 23:01:52 +0300

    Publishing balanced brackets.

---
+++ src/interview preparation kit/BalancedBrackets.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace HackerRank.interview_preparation_kit
+{
+    public class BalancedBrackets
+    {
+        // Complete the isBalanced function below.
+        public static string isBalanced(string s)
+        {
+            if (s.Length % 2 != 0) return "NO";
+
+            var stack = new Stack<char>();
+            foreach (var t in s)
+            {
+                switch (t)
+                {
+                    case ')':
+                        if (stack.Count != 0 && stack.Pop().Equals('(')) continue;
+                        return "NO";
+                    case '}':
+                        if (stack.Count != 0 && stack.Pop().Equals('{')) continue;
+                        return "NO";
+                    case ']':
+                        if (stack.Count != 0 && stack.Pop().Equals('[')) continue;
+                        return "NO";
+                    default:
+                        stack.Push(t);
+                        continue;
+                }
+            }
+
+            return stack.Count == 0 ? "YES" : "NO";
+        }
+    }
+}
+

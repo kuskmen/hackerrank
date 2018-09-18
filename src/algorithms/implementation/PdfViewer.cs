63fb69cbc479bfd27f3d8659f7ea316df807d034
Yanislav Vasilev <ianislav_vasilev@abv.bg>
2018-09-18 22:06:45 +0300

    Pdf viewer submission.

---
+++ src/algorithms/implementation/PdfViewer.cs
@@ -0,0 +1,21 @@
+﻿namespace HackerRank.algorithms.implementation
+{
+    public static class PdfViewer
+    {
+        public static int DesignerPdfViewer(int[] h, string word)
+        {
+            const string englishAlphabet = "abcdefghijklmnopqrstuvwxyz";
+            var maxHeight = 1;
+
+            foreach (var ch in word)
+            {
+                var height = h[englishAlphabet.IndexOf(ch)];
+                if (height > maxHeight)
+                {
+                    maxHeight = height;
+                }
+            }
+            return word.Length * maxHeight;
+        }
+    }
+}

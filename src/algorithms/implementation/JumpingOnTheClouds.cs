b90294679c855508e514e3b293caa0a133c32aa6
Yanislav Vasilev <ianislav_vasilev@abv.bg>
2018-10-09 18:30:09 +0300

    Commiting jumping on the clouds submission.

---
+++ src/algorithms/implementation/JumpingOnTheClouds.cs
@@ -0,0 +1,19 @@
+﻿namespace HackerRank.algorithms.implementation
+{
+    public class JumpingOnTheClouds
+    {
+        public static int jumpingOnClouds(int[] c, int k)
+        {
+            var energy = 100;
+            var nextCloudIndex = 0;
+
+            while (true)
+            {
+                nextCloudIndex += k;
+                energy -= c[nextCloudIndex % c.Length] == 1 ? 3 : 1;
+
+                if (nextCloudIndex % c.Length == 0) return energy;
+            }
+        }
+    }
+}

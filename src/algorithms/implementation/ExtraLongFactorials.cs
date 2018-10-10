2f31fd4010785a0cb69f427f5e339dfd40bb3db0
Yanislav Vasilev <ianislav_vasilev@abv.bg>
2018-10-10 18:49:04 +0300

    Extra long factorial submission.

---
+++ src/algorithms/implementation/ExtraLongFactorials.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace HackerRank.algorithms.implementation
+{
+    public class ExtraLongFactorials
+    {
+        public static void extraLongFactorials(int n)
+        {
+            var result = BigInteger.One;
+            while (n > 0)
+            {
+                result *= n;
+                n--;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
